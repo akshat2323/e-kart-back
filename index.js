8e7ffd98eedd89240aafa40359f96d691e1853f5
akshat bhatnagar <vickey.bhatnagar2323@gmail.com>
2022-06-18 00:48:24 +0530

    adding cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config({path: './config.env'});
 
 app.use(cors(
     {
-        origin: "http://127.0.0.1:3000/",
+        origin: "http://127.0.0.1:3000",
         methods:["GET" , "POST"],
         credentials: true,
     }
