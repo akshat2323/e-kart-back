7ea8d338809f6210d01af0ae55df7048ce5f5e8b
akshat bhatnagar <vickey.bhatnagar2323@gmail.com>
2022-06-24 17:55:20 +0530

    origin allow to local host

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 
 app.use(cors(
    {
-      origin:'*', 
+      origin:'http://localhost:3000', 
       credentials:true,            //access-control-allow-credentials:true
       optionSuccessStatus:200
   }
