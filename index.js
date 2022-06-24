4b149dbb676a6cca9c3cfdaee60762c2a00d67b3
akshat bhatnagar <vickey.bhatnagar2323@gmail.com>
2022-06-24 17:44:04 +0530

    adding cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 
 app.use(cors(
    {
-      origin:'http://localhost:3000', 
+      origin:'*', 
       credentials:true,            //access-control-allow-credentials:true
       optionSuccessStatus:200
   }
